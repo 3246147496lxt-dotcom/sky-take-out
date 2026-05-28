3ea250f9375c362defd78b5895369a1a3a413e74
3246147496lxt-dotcom <3246147496lxt@gmail.com>
2026-05-28 19:56:34 +0800

    删除菜品功能代码实现

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,15 +1,16 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -30,4 +31,25 @@ public class DishController {
         dishService.saveWithFlavor(dishDTO);
         return Result.success();
     }
+
+    /**
+     * 分页查询
+     */
+    @GetMapping("/page")
+    public Result page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("开始分页查询:{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 菜品批量删除
+     */
+    @DeleteMapping
+    public Result deleteDish(@RequestParam List<Long> ids){
+        log.info("菜品批量删除:{}", ids);
+        dishService.deleteDish(ids);
+        return Result.success();
+    }
+
 }
